7a55b624564bbd561cc3c2afa5a52df34b28be16
Taku Nakai <tnakaicode@gmail.com>
2019-09-03 11:34:32 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='pygrasp',
+      version='0.2',
+      description='Python package for GRASP interfacing.',
+      url='www.fluxanet.cl',
+      author='Pedro Fluxa Rojas',
+      author_email='pedro@fluxanet.cl',
+      license='GPL',
+      packages=['pygrasp', 'pygrasp.IO'],
+      zip_safe=False)
