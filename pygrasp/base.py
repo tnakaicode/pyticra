986b6b21a3bcf9842d5138fc6db317caf159bf7b
Taku Nakai <tnakaicode@gmail.com>
2019-12-20 13:28:16 +0900

    base rename

--- pygrasp/base.py
+++ pygrasp/base.py
@@ -119,15 +119,86 @@ class Physical(OrderedDict):
         return '{}({!r}, {!r}, {{{}}})'.format(self.__class__.__name__, self.display_name,
                                                self.class_name, ', '.join(['{!r}: {!r}'.format(k, v) for k, v in self.items()]))
 
+class Command(OrderedDict):
+    """
+    This class is a container for GRASP commands.
+
+    It inherits from OrderedDict so that it remembers the ordering of its properties.
+    """
+
+    def __init__(self, target_name, command_name, other={}):
+        super(Command, self).__init__(other)
+        self.target_name = str(target_name)
+        self.command_name = str(command_name)
+
+    def __str__(self):
+        lines = ['COMMAND OBJECT {} {}'.format(
+            self.target_name, self.command_name)]
+        lines.append('(')
+        for k, v in self.iteritems():
+            lines.append('  {:16s} : {},'.format(k, v))
+        lines[-1] = lines[-1].rstrip(',')
+        lines.append(')')
+        return ' &\n'.join(lines)
+
+    def __repr__(self):
+        return '{}({!r}, {!r}, {{{}}})'.format(self.__class__.__name__,
+                                               self.target_name,
+                                               self.command_name,
+                                               ', '.join(['{!r}: {!r}'.format(k, v) for k, v in self.iteritems()]))
+
+    # This code is shared between Command and Physical objects. Fix this.
+    def traverse(self, test, action):
+        """
+        Recursively visit all members of this object. See visit() for
+        parameter meanings.
+        """
+        for name, thing in self.iteritems():
+            self.visit(name, thing, test, action)
+
+    def visit(self, name, thing, test, action):
+        """
+        Recursively visit every member of this object, calling
+        action(name, thing) if test(name, thing) is True.
+        """
+        if test(name, thing):
+            action(name, thing)
+        elif isinstance(thing, Sequence):
+            for index, element in enumerate(thing):
+                self.visit(index, element, test, action)
+        elif isinstance(thing, Struct):
+            for key, value in thing.iteritems():
+                self.visit(key, value, test, action)
+
+
+class BatchCommand(list):
+
+    def __str__(self):
+        return ' '.join(self)
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__,
+                               super(BatchCommand, self).__repr__())
+
 
 class Grammar (object):
-    ident = p.Word(p.alphas + '_', p.alphanums + '_.')
+    """
+    This class contains the pyparsing grammar for the TICRA .tor and
+    .tci file formats.
+    """
+
+    # It might be cleaner to have the grammar for each class in the
+    # class itself, but this causes scope problems.
+    # Added '.' to handle Brad's EBEX sims. See if this breaks anything.
+    # An identifier is no longer a valid Python variable.
+    identifier = p.Word(p.alphas + '_', p.alphanums + '_.')
+    plus_or_minus = p.Literal('+') ^ p.Literal('-')
     value = p.Forward()
     s_int = p.Word(p.nums)
     s_flt = p.Word(p.nums + '.')
 
     number = p.Combine(
-        p.Optional(p.Literal('+') ^ p.Literal('-')) +
+        p.Optional(plus_or_minus) +
         p.Word(p.nums) +
         p.Optional('.' + p.Word(p.nums)) +
         p.Optional(p.CaselessLiteral('E') + p.Word(p.nums + '+-', p.nums)) +
@@ -136,9 +207,14 @@ class Grammar (object):
     quantity = (
         number +
         p.Optional(p.Word(p.alphas, p.alphanums + '-^/' + ' '), default=None) +
-        p.Optional(ident)
+        p.Optional(identifier)
     )
     quantity.setParseAction(lambda tokens: Quantity(*tokens))
+
+    value = p.Forward()
+    elements = p.delimitedList(value)
+    member = p.Group(identifier + p.Suppress(':') + value)
+    members = p.delimitedList(member)
 
     s_ref = (
         "ref" +
@@ -146,42 +222,53 @@ class Grammar (object):
         value +
         p.Suppress(")")
     )
-    s_ref.setParseAction(lambda tokens: [Ref(tokens[1])])
+    s_ref.setParseAction(lambda tokens: [Ref(tokens['ref'])])
 
-    s_seq_e = p.Combine(value + p.Optional(' ') + p.Optional(ident))
-    s_seq = (
+    s_sequence_elements = p.Combine(
+        value + p.Optional(' ') + p.Optional(identifier)
+    )
+    s_sequence = (
         "sequence" +
         p.Suppress("(") +
-        p.delimitedList(s_seq_e) +
+        p.delimitedList(s_sequence_elements) +
         p.Suppress(")")
     )
-    s_seq.setParseAction(lambda tokens: [Sequence(tokens[1:])])
+    s_sequence.setParseAction(
+        lambda tokens: [Sequence(tokens[1:])]
+    )
 
-    s_sct_e = p.Group(
-        ident +
+    s_struct_elements = p.Group(
+        identifier +
         p.Suppress(":") +
         p.Combine(
             value +
             p.Optional(' ') +
-            p.Optional(ident)))
-    s_sct = (
+            p.Optional(identifier))
+    )
+    s_struct = (
         "struct" +
         p.Suppress("(") +
-        p.delimitedList(s_sct_e) +
+        p.delimitedList(s_struct_elements) +
         p.Suppress(")")
     )
-    s_sct.setParseAction(
+    s_struct.setParseAction(
         lambda tokens: [Struct((dat[0], dat[1]) for dat in tokens[1:])]
     )
 
     comment = p.QuotedString('"', unquoteResults=False)
-    other = p.Word(p.alphanums + r'\/._-')
-    s_val = p.Combine('"' + value + '"' + p.Optional(' ') + p.Optional(ident))
-    value << (quantity | s_ref | s_val | s_sct | s_seq | other | comment)
 
-    member = p.Group(ident + p.Suppress(":") + value)
+    # This could be a filename or just a string. Added '\' to handle EBEX sim.
+    # Should convert to unix filename.
+    other = p.Word(p.alphanums + r'\/._-')
+    s_val = p.Combine(
+        '"' + value + '"' + p.Optional(' ') + p.Optional(identifier)
+    )
+    value << (quantity | s_ref | s_val | s_struct |
+              s_sequence | other | comment)
+
+    member = p.Group(identifier + p.Suppress(":") + value)
     physical = (
-        ident + ident +
+        identifier + identifier +
         p.Suppress('(') +
         p.Optional(p.delimitedList(member)) +
         p.Suppress(')')
@@ -195,3 +282,33 @@ class Grammar (object):
     object_repository = p.ZeroOrMore(physical) + p.StringEnd()
     object_repository.ignore(p.cppStyleComment)
     object_repository.ignore(p.pythonStyleComment)
+
+    command = (p.Suppress('COMMAND') +
+               p.Suppress('OBJECT') +
+               identifier('target_name') +
+               identifier('command_name') +
+               p.Suppress('(') +
+               p.Optional(members)('members') +
+               p.Suppress(')') +
+               p.CaselessLiteral('cmd_').suppress() +
+               p.Word(p.nums)('number'))
+    command.ignore(p.cppStyleComment)  # '// comment'
+    command.ignore(p.pythonStyleComment)  # '# comment'
+    command.ignore(p.Literal('&'))
+    command.setParseAction(lambda tokens: [Command(tokens['target_name'],
+                                                   tokens['command_name'],
+                                                   list(tokens.get('members', [])))])
+
+    # Add support for other batch commands.
+    batch_command = p.CaselessLiteral(
+        'FILES READ ALL') + other + p.LineEnd().suppress()
+    batch_command.setParseAction(lambda tokens: [BatchCommand(tokens)])
+    quit_command = p.CaselessLiteral('QUIT')
+
+    # Add support for multiple QUIT statements.
+    command_interface = (p.ZeroOrMore(batch_command)('batch_commands') +
+                         p.ZeroOrMore(command)('commands') +
+                         quit_command +
+                         p.StringEnd())
+    command_interface.ignore(p.cppStyleComment)
+    command_interface.ignore(p.pythonStyleComment)
