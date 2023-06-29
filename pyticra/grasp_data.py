ae42f7b53cac2ea3d57961c85fe70b586a20dba8
tnakaicode <tnakaicode@gmail.com>
2023-06-29 17:02:41 +0900

    GraspGridMulti

--- pyticra/grasp_data.py
+++ pyticra/grasp_data.py
@@ -156,18 +156,147 @@ class GraspGrid (GridBase):
                 (convert("m", self.unit)**2)
         print(self.filename, n0, n1)
 
+class GraspGridMulti (GridBase):
+
+    def __init__(self, filename, meta={}, freqs=[100.0], unit="mm", *args, **kwargs):
+        # Set the components dictionary (default empty)
+        self._meta = meta
+        self.filename = filename
+        self.fp = open(filename, "r")
+        self.id = filename.split("/")[-1].split(".")[-2]
+        self.unit = unit
+        self.freqs = freqs
+        self.freqs_txt = [f"{f:.1f}GHz" for f in freqs]
+        self.split_header()
+        self.initialize()
+
+        for name, data in meta.items():
+            self.__setitem__(name, data)
+
+        for key, value in kwargs.items():
+            self.__setattr__(key, value)
+
+    def __getitem__(self, name):
+        return self._meta[name]
+
+    def __setitem__(self, name, data):
+        self._meta[name] = data
+
+    def __delitem__(self, name):
+        del self._meta[name]
+
+    def __iter__(self):
+        return iter(self._meta)
+
+    def __len__(self):
+        return len(self._meta)
+
+    def __str__(self):
+        output = str(self.keys())
+        return output
+
+    def split_header(self, contxt="++++"):
+        self._meta['header'] = []
+        self._meta['header'].append(self.fp.readline().rstrip('\n'))
+        while self._meta['header'][-1] != contxt:
+            self._meta['header'].append(self.fp.readline().rstrip('\n'))
+
+    def initialize(self):
+        """
+        NSET   -  Number of field sets or beams.
+        ICOMP  -  Control parameter of field components.
+        NCOMP  -  Number of components.
+        IGRID  -  Control parameter of field grid type
+        
+        Grid Type
+            Spherical Far  (F1, F2)
+            Spherical Near (F1, F2, F3)
+            Planer    (F1, F2, F3)
+            Surface   (F1, F2, F3)            
+
+        line 3; NSET, ICOMP, NCOMP, IGRID
+        ICOMP =  1 "linear"          (Linear  ; E_rho,E_phi)
+        ICOMP =  2 "circular"        (Circular; rhc,lhc)
+        ICOMP =  3 "theta_phi"       (Linear  ; x,y)
+        ICOMP =  4 "major_minor"     (Major Minor; pol-elipse)
+        ICOMP =  5 "linear_xpd"      (XPD; E_rho/E_phi, E_phi/E_rho)
+        ICOMP =  6 "circular_xpd"    (XPD; rhc/lhc, lhc/rhc)
+        ICOMP =  7 "theta_phi_xpd"   (XPD; E_x/E_x, E_y/E_x)
+        ICOMP =  8 "major_minor_xpd" (XPD; major/minor, minor/major)
+        ICOMP =  9 "power"           (Power; |E|, sqrt(rhc/lhc))
+        ICOMP = 11 "poyting"         (Poynting)
+        """
+        self._meta['KTYPE'] = int(self.fp.readline().split()[0])
+        if self._meta['KTYPE'] != 1:
+            raise ValueError("Not implemented.")
+        nset, icomp, ncomp, igrid = [int(s)
+                                     for s in self.fp.readline().split()]
+        self._meta['NSET'] = nset
+        self._meta['ICOMP'] = icomp
+        self._meta['NCOMP'] = ncomp
+        self._meta['IGRID'] = igrid
+        for i in range(self._meta['NSET']):
+            self._meta['IX'], self._meta['IY'] = [int(s) for s in self.fp.readline().split()]
+        self._meta['XS'], self._meta['YS'], self._meta['XE'], self._meta['YE'] = [float(s) * convert(unit_in=self.unit, unit_out="mm") for s in self.fp.readline().split()]
+        self._meta['NX'], self._meta['NY'], self._meta['KLIMIT'] = [int(s) for s in self.fp.readline().split()]
+        if self._meta['KLIMIT'] != 0:
+            raise ValueError("Not implemented.")
+        self._meta['PX'] = np.linspace(
+            self._meta['XS'], self._meta['XE'], self._meta['NX'])
+        self._meta['PY'] = np.linspace(
+            self._meta['YS'], self._meta['YE'], self._meta['NY'])
+        self._meta['MESH'] = np.meshgrid(self._meta['PX'], self._meta['PY'])
+        self._meta['DX'] = (self._meta['XE'] -
+                            self._meta['XS']) / (self._meta['NX'] - 1)
+        self._meta['DY'] = (self._meta['YE'] -
+                            self._meta['YS']) / (self._meta['NY'] - 1)
+        self._meta['XCEN'] = self._meta['DX'] * self._meta['IX']
+        self._meta['YCEN'] = self._meta['DY'] * self._meta['IY']
+
+    def get_data(self, name, indx):
+        num = len(self._meta['header'])
+        num += self._meta['NSET'] + 4
+        row = self._meta['NX'] * self._meta['NY']
+        n0 = num + indx * (row + 2)
+        n1 = n0 + row
+        rows = range(n0, n1)
+        data = get_grd(self.filename, rows)
+        if self.id == "cur":
+            coef = 1 / self.knum * (np.sqrt(self.z_0 / 2))
+            # coef = 1.0
+            self._meta[name] = get_cur(
+                self._meta, data, coef=1 / coef) / (convert("m", self.unit)**2)
+        elif self.id == "e":
+            coef = 1 / (self.knum * np.sqrt(2 * self.z_0))
+            # coef = 1.0
+            self._meta[name] = get_e(
+                self._meta, data, coef=1 / coef) / (convert("m", self.unit)**2)
+        elif self.id == "h":
+            coef = 1 / (self.knum) * np.sqrt(self.z_0 / 2)
+            # coef = 1.0
+            self._meta[name] = get_h(
+                self._meta, data, coef=1 / coef) / (convert("m", self.unit)**2)
+        elif self.id == "pw":
+            coef = 1.0
+            self._meta[name] = get_pw(
+                self._meta, data, coef=1 / coef) / (convert("m", self.unit)**2)
+        else:
+            coef = 1.0
+            self._meta[name] = get_e(self._meta, data) / \
+                (convert("m", self.unit)**2)
+        print(self.filename, n0, n1)
 
 class TicraSlipOver(GridBase):
 
-    def __init__(self, filename, meta={}, freqs=["104.0GHz"], *args, **kwargs):
+    def __init__(self, filename, meta={}, freqs=[100.0], *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # Set the components dictionary (default empty)
         self._meta = meta
         self.filename = filename
         self.fp = open(filename, "r")
-        self.id = filename.split("/")[-1].split(".")[-2]
         self.freqs = freqs
+        self.freqs_txt = [f"{f:.1f}GHz" for f in freqs]
         self.split_header()
         self.initialize()
 
