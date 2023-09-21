5f110b8db2ae010d747d731c6518f5caba5ced69
tnakaicode <tnakaicode@gmail.com>
2023-09-21 10:02:17 +0900

    update grammar

--- pyticra/base.py
+++ pyticra/base.py
@@ -94,6 +94,16 @@ class Struct(OrderedDict):
                 OrderedDict, self).__repr__())
 
 
+class table(list):
+
+    def __str__(self):
+        return 'table(\n{}\n)'.format('\n'.join(["\t".join(s) for s in line] for line in self[0]))
+
+    def __repr__(self):
+        return '{}(\n{}\n)'.format(self.__class__.__name__,
+                                   super(table, self).__repr__())
+
+
 class Physical(OrderedDict):
     """
     This class is a lightweight container for GRASP physical objects.
@@ -185,7 +195,31 @@ class BatchCommand(list):
                                super(BatchCommand, self).__repr__())
 
 
-class Grammar (object):
+class Function(OrderedDict):
+    """
+    This class is a container for GRASP functions.
+
+    It inherits from OrderedDict so that it remembers the ordering of its properties.
+    """
+
+    def __init__(self, function_name, other={}):
+        super(Function, self).__init__(other)
+        self.function_name = str(function_name)
+
+    def __str__(self):
+        lines = ['FUNCTION {}'.format(self.function_name)]
+        for v in self.items():
+            lines.append('\t{}'.format(Command(v[0], v[1], v[2:])))
+        lines.append('END')
+        return ' &\n'.join(lines)
+
+    def __repr__(self):
+        return '{}({!r}, {!r}, {{{}}})'.format(self.__class__.__name__,
+                                               self.function_name,
+                                               '\t'.join(['{}'.format(Command(v[0], v[1], v[2:])) for v in self.items()]))
+
+
+class Grammar(object):
     """
     This class contains the pyparsing grammar for the TICRA .tor and
     .tci file formats.
@@ -193,16 +227,15 @@ class Grammar (object):
 
     # It might be cleaner to have the grammar for each class in the
     # class itself, but this causes scope problems.
-    # Added '.' to handle Brad's EBEX sims. See if this breaks anything.
-    # An identifier is no longer a valid Python variable.
-    identifier = p.Word(p.alphas + '_', p.alphanums + '_.')
-    plus_or_minus = p.Literal('+') ^ p.Literal('-')
+    ident = p.Word(p.alphas + '_', p.alphanums + '_.')
     value = p.Forward()
+    empty = p.Empty()
     s_int = p.Word(p.nums)
     s_flt = p.Word(p.nums + '.')
 
+    plus_or_minus = p.Literal('+') ^ p.Literal('-')
     number = p.Combine(
-        p.Optional(plus_or_minus) +
+        p.Optional(p.Literal('+') ^ p.Literal('-')) +
         p.Word(p.nums) +
         p.Optional('.' + p.Word(p.nums)) +
         p.Optional(p.CaselessLiteral('E') + p.Word(p.nums + '+-', p.nums)) +
@@ -211,68 +244,111 @@ class Grammar (object):
     quantity = (
         number +
         p.Optional(p.Word(p.alphas, p.alphanums + '-^/' + ' '), default=None) +
-        p.Optional(identifier)
+        p.Optional(ident)
     )
     quantity.setParseAction(lambda tokens: Quantity(*tokens))
 
-    value = p.Forward()
-    elements = p.delimitedList(value)
-    member = p.Group(identifier + p.Suppress(':') + value)
-    members = p.delimitedList(member)
+    # Added '.' to handle Brad's EBEX sims. See if this breaks anything.
+    # An identifier is no longer a valid Python variable.
 
+    # "ref(val_name)"
     s_ref = (
         "ref" +
         p.Suppress("(") +
         value +
-        p.Suppress(")")
+        p.Suppress(")") +
+        p.Optional(ident)
     )
     s_ref.setParseAction(lambda tokens: [Ref(tokens[1])])
 
-    s_sequence_elements = p.Combine(
-        value + p.Optional(' ') + p.Optional(identifier)
-    )
-    s_sequence = (
-        "sequence" +
+    # "ref(val_name)" mm
+    s_ref_unit = p.Combine(
+        "ref" +
         p.Suppress("(") +
-        p.delimitedList(s_sequence_elements) +
+        value +
+        p.Suppress(")") +
+        value
+    )
+    s_ref_unit.setParseAction(lambda tokens: [Ref(tokens[1])])
+
+    # "ref()"
+    s_ref_empty = (
+        "ref" +
+        p.Suppress("(") +
         p.Suppress(")")
     )
-    s_sequence.setParseAction(
-        lambda tokens: [Sequence(tokens[1:])]
-    )
+    s_ref_empty.setParseAction(lambda tokens: [Ref("")])
 
-    s_struct_elements = p.Group(
-        identifier +
+    # sequence(ref(val1), ref(val2), 1.0)
+    s_seq_element = p.Combine(value + p.Optional(' ') + p.Optional(ident))
+    s_seq = (
+        "sequence" +
+        p.Suppress("(") +
+        p.delimitedList(s_seq_element) +
+        p.Suppress(")")
+    )
+    s_seq.setParseAction(lambda tokens: [Sequence(tokens[1:])])
+
+    # struct(v1: 0.0 mm, v2:  "ref(v2)", v3:  "ref(v3_1)+ref(v3_2)")
+    s_struct_element = p.Group(
+        ident +
         p.Suppress(":") +
         p.Combine(
             value +
             p.Optional(' ') +
-            p.Optional(identifier))
+            p.Optional(ident)
+        )
     )
+
     s_struct = (
         "struct" +
         p.Suppress("(") +
-        p.delimitedList(s_struct_elements) +
+        p.delimitedList(s_struct_element) +
         p.Suppress(")")
     )
     s_struct.setParseAction(
         lambda tokens: [Struct((dat[0], dat[1]) for dat in tokens[1:])]
     )
 
-    comment = p.QuotedString('"', unquoteResults=False)
-
-    # This could be a filename or just a string. Added '\' to handle EBEX sim.
-    # Should convert to unix filename.
-    other = p.Word(p.alphanums + r'\/._-')
-    s_val = p.Combine(
-        '"' + value + '"' + p.Optional(' ') + p.Optional(identifier)
+    #  table
+    #    (
+    #    Lx  "ref(Lx)"      1
+    #    Ly  "ref(Ly)"      1
+    #    Wx  "ref(Wx)"      1
+    #    Wy  "ref(Wy)"      1
+    #    Lx  "ref(Lx)"      2
+    #    Ly  "ref(Ly)"      2
+    #    Wx  "ref(Wx)"      2
+    #    Wy  "ref(Wy)"      2
+    #    )
+    s_table_line = p.Group(p.OneOrMore(value, stopOn=p.LineEnd()))
+    s_table_element = p.OneOrMore(s_table_line)
+    s_table = (
+        "table" +
+        p.Suppress("(") +
+        s_table_element +
+        p.Suppress(")")
     )
-    value << (quantity | s_ref | s_val | s_struct |
-              s_sequence | other | comment)
+    s_table.setParseAction(lambda tokens: [table(tokens[1:])])
 
-    member = p.Group(identifier + p.Suppress(":") + value)
+    s_table_empty = (
+        "table" +
+        p.Suppress("(") +
+        p.Suppress(")")
+    )
+    s_table.setParseAction(lambda tokens: [table("")])
+
+    comment = p.QuotedString('"', unquoteResults=False)
+    other = p.Word(p.alphanums + r'\/._-')
+    s_val = p.Combine('"' + value + '"' + p.Optional(' ') + p.Optional(ident))
+
+    value << (quantity | s_val |
+              s_ref_empty | s_ref | s_ref_unit |
+              s_struct | s_seq | s_table | s_table_empty |
+              other | comment)
+    member = p.Group(ident + p.Suppress(":") + value)
     physical = (
-        identifier + identifier +
+        ident + ident +
         p.Suppress('(') +
         p.Optional(p.delimitedList(member)) +
         p.Suppress(')')
@@ -289,28 +365,46 @@ class Grammar (object):
 
     command = (p.Suppress('COMMAND') +
                p.Suppress('OBJECT') +
-               identifier +
-               identifier +
+               ident('target_name') +
+               ident('command_name') +
                p.Suppress('(') +
-               p.Optional(p.delimitedList(member)) +
-               p.Suppress(')') +
-               p.CaselessLiteral('cmd_').suppress() +
-               p.Word(p.nums)('number'))
+               p.Optional(p.delimitedList(member))('member') +
+               p.Suppress(')'))
     command.ignore(p.cppStyleComment)  # '// comment'
     command.ignore(p.pythonStyleComment)  # '# comment'
-    command.ignore(p.Literal('&\n'))
-    command.setParseAction(
-        lambda tokens: [Command(tokens[0], tokens[1], tokens[2:])]
-    )
+    command.ignore(p.Literal('&'))
+    command.setParseAction(lambda tokens: [Command(tokens['target_name'],
+                                                   tokens['command_name'],
+                                                   tokens[2:])])
+    command.set_name('command')
+
+    function = (p.Suppress('FUNCTION') +
+                ident('function_name') +
+                p.Optional(p.delimitedList(command))('command_object') +
+                p.Suppress('END'))
+    function.ignore(p.cppStyleComment)  # '// comment'
+    function.ignore(p.pythonStyleComment)  # '# comment'
+    function.ignore(p.Literal('&'))
+    function.setParseAction(lambda tokens: [Function(tokens['function_name'],
+                                                     tokens[1:])])
+    function.set_name('function')
+
+    function_call = (ident('function_name') +
+                     p.Suppress('(') +
+                     p.Suppress(')'))
+    function_call.set_name('function_call')
 
     # Add support for other batch commands.
-    batch_command = p.CaselessLiteral(
-        'FILES READ ALL') + other + p.LineEnd().suppress()
+    batch_command = (p.CaselessLiteral('FILES READ ALL') +
+                     other +
+                     p.LineEnd().suppress())
     batch_command.setParseAction(lambda tokens: [BatchCommand(tokens)])
     quit_command = p.CaselessLiteral('QUIT')
+    group_command = p.Group((command | function | function_call))
 
     # Add support for multiple QUIT statements.
-    command_interface = p.ZeroOrMore(command)('commands') + quit_command
+    command_interface = (p.ZeroOrMore(group_command) +
+                         p.StringEnd())
     command_interface.ignore(p.cppStyleComment)
     command_interface.ignore(p.pythonStyleComment)
 
--- scripts/test_load_tor.py
+++ scripts/test_load_tor.py
@@ -38,8 +38,8 @@ if __name__ == '__main__':
         shutil.copyfile(tor_file, "./torfile/" + name + ".tor")
 
 
-#  0 Analysis_and_Optimisation_of_PUC Expected end of text, found 'loop'  (at char 439), (line:21, col:1)
-#  1 Analysis_of_PUC Expected end of text, found 'periodic'  (at char 285), (line:17, col:1)
+#  0 Analysis_and_Optimisation_of_PUC
+#  1 Analysis_of_PUC
 #  2 Array_Fed_Reflector
 #  3 BoR_MoM_with_Offset_Feed
 #  4 CouplingBetweenTwoAntennas
@@ -50,43 +50,43 @@ if __name__ == '__main__':
 #  9 Dual_Gridded_Refl_with_Stiffeners
 # 10 Dual_Gridded_Refl_with_Support_Ring
 # 11 Dual_Refl_with_Blockage
-# 12 Dual_Refl_with_Panels_and_Struts Expected end of text, found 'main'  (at char 30), (line:5, col:1)
-# 13 Dual_Refl_with_Shaped_Surfaces Expected end of text, found 'double'  (at char 3162), (line:129, col:1)
+# 12 Dual_Refl_with_Panels_and_Struts
+# 13 Dual_Refl_with_Shaped_Surfaces
 # 14 Ext_Command_Plugin
 # 15 FrontEnd_Design_Cassegrain
-# 16 Helixes_Radome_Satellite Expected end of text, found 'radome'  (at char 6177), (line:224, col:1)
-# 17 Horn_with_Lens Expected end of text, found 'lens'  (at char 577), (line:24, col:1)
+# 16 Helixes_Radome_Satellite
+# 17 Horn_with_Lens
 # 18 Intro_Optimisation
 # 19 Lens_Refractor
 # 20 Matched_Feed Expected end of text, found 'mode'  (at char 5559), (line:186, col:1)
 # 21 MGTD_Dual_Reflector
-# 22 NASA_Almond Expected end of text, found 'composite'  (at char 660), (line:34, col:1)
+# 22 NASA_Almond
 # 23 Offset_Reflector_in_Radome
-# 24 Optimisation_of_Reflectarray Expected end of text, found 'periodic'  (at char 286), (line:17, col:1)
+# 24 Optimisation_of_Reflectarray
 # 25 Optim_and_Analysis_of_Multibeam_Direct_Radiating_Array
 # 26 Optim_and_analysis_of_multibeam_reflector
 # 27 Optim_of_QZ_in_CATR
-# 28 Orthomode_Transducer Expected end of text, found 'cad'  (at char 6822), (line:305, col:1)
+# 28 Orthomode_Transducer Expected end of text, found 'rwg'  (at char 11295), (line:440, col:1)
 # 29 Parameter_Sweep
-# 30 Parametric_Helix_Antenna Expected end of text, found 'Bend'  (at char 0), (line:1, col:1)
-# 31 Patch_Antenna_with_Dielectric Expected end of text, found 'patch'  (at char 1454), (line:59, col:1)
-# 32 Platform_Scattering_from_CubeSat Expected end of text, found 'cad'  (at char 1713), (line:69, col:1)
-# 33 Potter_Horn Expected end of text, found 'circular'  (at char 2962), (line:120, col:1)
-# 34 PUC3D_tutorial Expected end of text, found 'Rectangular'  (at char 1787), (line:92, col:1)
-# 35 QuasiOpticalFrame Expected end of text, found 'Frame'  (at char 0), (line:1, col:1)
-# 36 Reflector_with_Corrugated_Horn Expected end of text, found 'horn'  (at char 618), (line:25, col:1)
+# 30 Parametric_Helix_Antenna
+# 31 Patch_Antenna_with_Dielectric
+# 32 Platform_Scattering_from_CubeSat __init__() takes from 2 to 3 positional arguments but 4 were given
+# 33 Potter_Horn
+# 34 PUC3D_tutorial
+# 35 QuasiOpticalFrame
+# 36 Reflector_with_Corrugated_Horn
 # 37 Ridge_Waveguide_Devices Expected end of text, found 'Horn'  (at char 730), (line:35, col:1)
-# 38 Setup_and_Analysis_of_Reflectarray Expected end of text, found 'periodic'  (at char 1675), (line:71, col:1)
-# 39 Single_Offset_Shaped_Reflector_for_Cosecant_Beam Expected end of text, found 'pattern'  (at char 2782), (line:127, col:1)
+# 38 Setup_and_Analysis_of_Reflectarray
+# 39 Single_Offset_Shaped_Reflector_for_Cosecant_Beam
 # 40 Single_Refl_with_Three_Struts
 # 41 Single_Shaped_Reflector_in_CP_and_Tabulated_Rim
 # 42 Sing_Refl_Shap_LeastSquares
 # 43 Sing_Refl_Shap_rim_indent_curv_cons
-# 44 Standard_Gain_Horn_with_Waveguide_Input Expected end of text, found 'cwg'  (at char 4342), (line:181, col:1)
+# 44 Standard_Gain_Horn_with_Waveguide_Input
 # 45 UQ_Analysis_of_Multibeam_Direct_Radiating_Array
 # 46 UQ_axial_horn Expected end of text, found 'axial'  (at char 1093), (line:52, col:1)
-# 47 UQ_Large_Deployable_Mesh_Reflector Expected end of text, found 'irregular'  (at char 2609), (line:117, col:1)
-# 48 UQ_Patch_Antenna Expected end of text, found 'patch'  (at char 1454), (line:59, col:1)
-# 49 UQ_Reflectarray Expected end of text, found 'periodic'  (at char 1768), (line:74, col:1)
+# 47 UQ_Large_Deployable_Mesh_Reflector
+# 48 UQ_Patch_Antenna
+# 49 UQ_Reflectarray
 # 50 User_Terminal_Antenna Expected end of text, found 'port'  (at char 2144), (line:80, col:1)
-# 51 Waveguide_Fed_Horn_with_Offset_Reflector Expected end of text, found 'Axially'  (at char 724), (line:35, col:1)
+# 51 Waveguide_Fed_Horn_with_Offset_Reflector
