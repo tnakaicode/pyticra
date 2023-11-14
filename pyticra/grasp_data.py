4f4ad99e28ff0a3aa0ab51585eea48d9d74f163d
tnakaicode <tnakaicode@gmail.com>
2023-11-14 09:15:15 +0900

    fix import error MutableMapping

--- pyticra/grasp_data.py
+++ pyticra/grasp_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.constants as cnt
 from abc import abstractmethod
-from collections import MutableMapping
+from collections.abc import MutableMapping
 
 from pyticra.units import convert
 
