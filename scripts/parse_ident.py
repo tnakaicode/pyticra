6821a2ec444a27ced1a2c5420f8143c0bdc06917
tnakaicode <tnakaicode@gmail.com>
2023-12-21 19:11:46 +0900

    fix error

--- scripts/parse_ident.py
+++ scripts/parse_ident.py
@@ -12,13 +12,22 @@ body = key + suite
 pattern = (Word(alphas) + Suppress("(") + Word(alphas) + Suppress(")"))
 stmt << pattern
 
+
 def key_parse_action(toks):
     print("Parsing '%s'..." % toks[0])
 
+
 key.setParseAction(key_parse_action)
 header = Suppress("[") + Literal("test") + Suppress("]")
-
 content = (header + OneOrMore(indentedBlock(body, indent_stack, False)))
-parser = OneOrMore(content)
+
+contents = Forward()
+suites = indentedBlock(content, indent_stack)
+
+extra = Literal("extra") + Suppress(":") + suites
+contents << (content | extra)
+
+parser = OneOrMore(contents)
 
 parser.parseFile("./scripts/parse_ident.txt", parseAll=True)
+print(parser)
